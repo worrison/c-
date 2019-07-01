50c111ef178f11ca2386cd9a46cf00b3e6ad443a
formacion <formacion@gtt.es>
2019-07-01 12:09:42 +0200

    ejercicios

---
+++ 2bisiesto/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _2bisiesto
+{
+    class Program
+    {
+        static bool bisiesto(int fecha){
+            return fecha % 400 == 0 || fecha %4 == 0 && fecha %4 != 0;
+        }
+        static void Main(string[] args)
+        {
+            int fecha = Int32.Parse(args[0]);
+            bool resultado=bisiesto(fecha);
+            Console.WriteLine(resultado ? "es bisiesto":"no es bisiesto");
+        }
+    }
+}

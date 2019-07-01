136077247471abb08e042444dc2da4b03ac9fadc
formacion <formacion@gtt.es>
2019-07-01 14:25:44 +0200

    media piramide con el padrigth

---
+++ piramide/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace piramide
+{
+    class Program
+    {
+        static void piramide (int elementos)
+        {
+            int i=0;
+            string texto="";
+            char caracter='*',espacio=' ';
+            while(i<=elementos)
+            {
+                texto=texto.PadRight(i, caracter);
+                texto=texto.PadLeft(elementos-i,espacio);
+                
+                Console.WriteLine(texto);
+                i++;
+            }
+        }
+        static void Main(string[] args)
+        {
+            piramide(Int32.Parse(args[0])); 
+        }
+    }
+}

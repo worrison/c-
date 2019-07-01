d3632d10bef63e532384a26f0b31c3a3ba9ecc4d
formacion <formacion@gtt.es>
2019-07-01 14:28:43 +0200

    media piramide

---
+++ 3media/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _3media
+{
+    class Program
+    {
+        static float media(string[] numeros,int cantidad)
+        {
+            int i;
+            int suma=0;
+            float resultado;
+            for(i=0; i<cantidad; i++)
+            {
+                suma+=Int32.Parse(numeros[i]);
+            }
+            return resultado=suma/2;
+        }
+        static void Main(string[] args)
+        {
+            int cantidad= args.Length;
+            float resultado=media(args,cantidad);
+            
+            Console.WriteLine(resultado);
+        }
+    }
+}

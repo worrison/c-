9010ad7f94a0b324cd4f355f35feaaad2a83373a
formacion <formacion@gtt.es>
2019-07-01 14:57:11 +0200

    numeros romanos pero con errores

---
+++ numerosRoanos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace numerosRoanos
+{
+    class Program
+    {
+        static string romanos(int numeros)
+        {
+            string numeroRomano="";
+            while (numeros > 0)
+            {
+                switch (numeros)
+                {
+                    case numeros >=1000:
+                        {
+                            numeros-=1000;
+                            numeroRomano+="M";
+                            break;
+                        };
+                    case numeros >= 500:
+                        {
+                            numeros-=500;
+                            numeroRomano+="D";
+                            break;
+                        }
+                }
+            }
+            return numeroRomano;
+
+        }
+        static void Main(string[] args)
+        {
+            int numeroAConvertir = Int32.Parse(args[0]);
+            string nuemroRomano = romanos(numeroAConvertir);
+        }
+    }
+}

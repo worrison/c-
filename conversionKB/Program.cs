30524396f9e696fdfce14e642a8dbd26bd7ec2ca
formacion <formacion@gtt.es>
2019-07-02 13:55:48 +0200

    conversion de kb a mb

---
+++ conversionKB/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+// 4º Actividad.
+// Realizar un programa que no pida un unidad en kb y nos haga la conversión a MB
+namespace conversionKB
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un numero en Kb a convertir a MB");
+            double kb=Double.Parse(Console.ReadLine());
+            double mb=kb/1024;
+            Console.WriteLine("La conversion en Mb es:"+mb);
+        }
+    }
+}

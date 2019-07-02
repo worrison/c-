dc88ea695a1d7a7162fb429e0e1c6fea3943da8a
formacion <formacion@gtt.es>
2019-07-02 13:24:53 +0200

    array a la inversa

---
+++ numerosInversa/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace numerosInversa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tamano=5,i;
+            string[] inversa=new string[tamano];
+            for(i=0; i<tamano;i++)
+            {
+                inversa[i]=args[tamano-i-1];
+            }
+            Console.WriteLine(string.Join("\n", inversa));
+        }
+    }
+}

bfd2ce1a451ad944794997f403c9ef715234cf30
victor ordaxm <victorordaxantolin@gmail.com>
2019-07-02 21:42:24 +0200

    potencias y fibonacci

---
+++ fibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, limite, i, aux; 
+            Console.WriteLine("Ingrese el número de numeros de Fibonacci que desea mostrar");
+            limite = int.Parse(Console.ReadLine()); 
+            a = 0;
+            b = 1; 
+            for (i = 0; i < limite; i++)  
+            {
+                aux = a;
+                a = b; 
+                b = aux + a; 
+                Console.WriteLine(a); 
+            }
+        }
+    }
+}
---
+++ potencia/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace potencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca Base");
+            double Base=double.Parse(Console.ReadLine());
+            System.Console.WriteLine("introduzca exponencia");
+            double exponencia=double.Parse(Console.ReadLine());
+            double resultado=Math.Pow(Base,exponencia);
+            System.Console.WriteLine("Resultado "+resultado);
+        }
+    }
+}

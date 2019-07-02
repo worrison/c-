3b252a4c7f76ad8cf2b072bb3beaade9578908cf
victor ordaxm <victorordaxantolin@gmail.com>
2019-07-02 23:18:11 +0200

    cantidad de positivos y negativos

---
+++ positivosnegativos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace positivosnegativos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int positivos=0,negativos=0,num=0;
+            foreach (string numero in args)
+            {
+                num=Int32.Parse(numero);
+                if(num != 0)
+                {
+                    num>0 ? positivos++ : negativos++;
+                }
+            }
+           
+            System.Console.WriteLine("Positivos: "+positivos+"negativos: "+negativos);
+        }
+    }
+}

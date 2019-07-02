d2971467d76d02dcdd6c040c969f50a5ee313d72
victor ordaxm <victorordaxantolin@gmail.com>
2019-07-03 00:05:02 +0200

    quiniela

---
+++ quiniela/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace quiniela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int apuestas=15;
+            int[] quiniela1= new int[15];
+            int[] quiniela2= new int[15];
+            int[] quiniela3= new int[15];
+            int randomNumber;
+            for (int i = 0; i < 15; i++)
+            {
+                randomNumber=rnd.Next(0,100);
+                if(randomNumber>50)
+                {
+                    quiniela1[i]=2;
+                    quiniela2[i]=1;
+                    quiniela3[i]=1;
+                }else if(randomNumber<50)
+                {
+                    quiniela1[i]=1;
+                    quiniela2[i]=2;
+                    quiniela3[i]=0;
+                }else{
+                    quiniela1[i]=0;
+                    quiniela2[i]=1;
+                    quiniela3[i]=0;
+                }
+            }
+            Console.WriteLine("Primera Quiniela: "+string.Join("", quiniela1));
+            Console.WriteLine("Segunda Quiniela: "+string.Join("", quiniela2));
+            Console.WriteLine("Tercera Quiniela: "+string.Join("", quiniela3));
+        }
+    }
+}

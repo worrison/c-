4a6a0b5fa52c81f3c3f18338aced191e0e501778
victor ordaxm <victorordaxantolin@gmail.com>
2019-07-02 21:09:01 +0200

    media de numeros

---
+++ mediaNumeros/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace mediaNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tamano=args.Length,suma=0;
+
+            for(int i=0; i< tamano; i++)
+            {
+                System.Console.WriteLine(args[i]);
+                suma+=Int32.Parse(args[i]);
+            }
+            Console.WriteLine("Media: "+suma/tamano);
+        }
+    }
+}

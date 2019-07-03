9e565a2a9a019968f92fabac27ada83a03c57c06
formacion <formacion@gtt.es>
2019-07-03 13:30:16 +0200

    ejercicio de pruebas de clases y objetos

---
+++ vehiculos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace vehiculos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            vehiculo manolo= new vehiculo();
+            vehiculo manolo2= new vehiculo(9);
+            manolo.setNRuedas(4);
+            manolo.setMotor("Gasolina");
+            manolo.setCapacidad(8);
+            // manolo.setColor("#000");
+            manolo.setNumPuertas(2);
+            manolo.setVelocidadMaxima(250);
+            manolo.isIsAutomatico(false);
+            
+            manolo.parar();
+            manolo.arrancar();
+            manolo.arrancar();
+            manolo.parar();
+            bool devuelto=manolo2.comprobarBastidor("#1253498765");
+            System.Console.WriteLine(manolo.getCapacidad()+manolo.getColor()+manolo2.getNumPuertas()+devuelto);
+        }
+    }
+}

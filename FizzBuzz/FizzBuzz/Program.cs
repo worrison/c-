e5568218c049464b8cfa559014b680111bcb0b69
victor ordaxm <victorordaxantolin@gmail.com>
2019-07-10 23:01:06 +0200

    ejercicio FizzBuzz

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+/*Escribe un programa que imprima los número del 1 al 100, pero aplicando las siguientes normas: 
+ * Devuelve Fizz si el número es = 3 o divisible por 3. 
+ * Devuelve Buzz si el número es = 5 o divisible por 5. 
+ * Devuelve FizzBuzz si el número es divisible por 3 y por 5.
+ * */
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int INICIO = 1, FIN = 100;
+
+            for(int i=INICIO;i<=FIN;i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0 )
+                {
+                    Console.WriteLine("FizzBuzz");
+                }else if(i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }else if(i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+        }
+    }
+}
